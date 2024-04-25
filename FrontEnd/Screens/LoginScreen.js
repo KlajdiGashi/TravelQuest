6563ceb5705c6fe029ace389281002433fa1213b
Klajdi Gashi <klajdi.gashi@student.uni-pr.edu>
2024-04-25 15:14:10 +0200

    TQ-1:Change UI Design

--- FrontEnd/Screens/LoginScreen.js
+++ FrontEnd/Screens/LoginScreen.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity,Image} from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
+import LogoImage from '../Logo.png'
 
 export default function LoginScreen({ navigation }) {
   const [username, setUsername] = useState('');
@@ -28,7 +29,11 @@ export default function LoginScreen({ navigation }) {
     <View style={styles.container}>
       <View style={styles.backgroundTop} />
       <View style={styles.logoContainer}>
-        {/*  logo component  */}
+      <Image
+      source={{ uri: 'https://marblism-dashboard-api--production-public.s3.us-west-1.amazonaws.com/ax6Rka-travelquest-JhBc' }}
+      style={{ width: 200, height: 100, borderRadius:10,bottom:60,shadowOffset:{width:0,height:2,},shadowOpacity:2,shadowRadius:4.84,elevation:5,}} 
+      resizeMode="contain" 
+      />
       </View>
       <View style={styles.inputContainer}>
         <Text style={styles.label}>Username:</Text>
@@ -63,7 +68,7 @@ export default function LoginScreen({ navigation }) {
       </TouchableOpacity>
       <View style={styles.buttonContainer}>
         <TouchableOpacity onPress={handleSignup}>
-          <Text style={styles.buttonText}>Sign Up</Text>
+          <Text style={styles.SignUpText}>Sign Up</Text>
         </TouchableOpacity>
         <TouchableOpacity onPress={handleForgotPassword}>
           <Text style={styles.forgotPasswordText}>Forgot Password?</Text>
@@ -76,18 +81,18 @@ export default function LoginScreen({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#F5F5F5',
+    backgroundColor: '#121212',
     alignItems: 'center',
     justifyContent: 'center',
     paddingHorizontal: 20,
   },
   backgroundTop: {
     position: 'absolute',
-    top: -100,
-    left: -200,
+    top: -500,
+    left: -280,
     right: -200,
     bottom: '60%', 
-    backgroundColor: '#005C99', 
+    backgroundColor: '#F5F5F5', 
     transform: [{ rotateZ:'-30deg' }],
   },
   logoContainer: {
@@ -96,61 +101,43 @@ const styles = StyleSheet.create({
   inputContainer: {
     width: '100%',
     marginBottom: 20,
+    borderRadius: 10,
+    overflow: 'hidden',
   },
   label: {
     marginBottom: 5,
-    color: '#F5F5F5',
+    color: '#000000',
   },
   input: {
     width: '100%',
     height: 50,
     borderRadius: 10,
     paddingHorizontal: 15,
-    backgroundColor: 'white',
+    backgroundColor: '#FAFAFA',
     fontSize: 18,
-    shadowOffset:{
+    shadowColor: '#000', // Dark shadow color
+    shadowOffset: {
       width: 0,
-      height: 2,
+      height: 2, 
     },
-    shadowOpacity:0.25,
-    shadowRadius:3.84,  
-    elevation:5,
+    shadowOpacity: 2,
+    shadowRadius: 4.68, 
+    elevation: 5,
   },
+  
   passwordContainer: {
     flexDirection: 'row',
     alignItems: 'center',
     borderRadius: 10,
     paddingHorizontal: 15,
-    backgroundColor: 'white',
-    shadowOffset:{
-      width: 0,
-      height: 2,
+    backgroundColor: '#FAFAFA',
+    shadowOffset: {
+      width: 1,
+      height: 0,
     },
-    shadowOpacity:0.25,
-    shadowRadius:3.84,  
-    elevation:5,
-  },
-  passwordInput: {
-    flex: 1,
-    height: 50,
-    fontSize: 18,
-    
-  },
-  eyeIcon: {
-    position: 'absolute',
-    top: 14, 
-    right: 10,
-  },
-  buttonContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    width: '100%',
-    marginTop: 10,
-  },
-  buttonText: {
-    color: '#005C99',
-    textDecorationLine: 'underline',
-    fontSize: 18,
+    shadowOpacity: 0.50,
+    shadowRadius: 3.84,
+    elevation: 5,
   },
   loginButton: {
     backgroundColor: '#005C99',
@@ -160,9 +147,54 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     borderRadius: 10,
     marginTop: 10,
+    shadowOffset: {
+      width: 0,
+      height: 0,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  
+  passwordInput: {
+    flex: 1,
+    height: 50,
+    fontSize: 18,
+    
+  },
+  eyeIcon: {
+    position: 'absolute',
+    top: 13, 
+    right: 10,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '100%',
+    marginTop: 10,
+  },
+  buttonText: {
+    color: '#000000',
+    textDecorationLine: 'underline',
+    fontSize: 18,
+  },
+  loginButton: {
+    backgroundColor: '#F5F5F5',
+    width: '100%',
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10,
+    marginTop: 10,
   },
   forgotPasswordText: {
-    color: '#005C99',
+    color: '#F5F5F5',
+    textDecorationLine: 'underline',
+    fontSize: 16,
+    
+  },
+  SignUpText: {
+    color: '#F5F5F5',
     textDecorationLine: 'underline',
     fontSize: 16,
     
