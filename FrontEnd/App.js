342b184939f6372fbcd0a6b830a7f3ba40ae218a
Klajdi Gashi <klajdi.gashi@student.uni-pr.edu>
2024-04-28 22:32:09 +0200

    Feat:Fix Main Navigation functionality

--- FrontEnd/App.js
+++ FrontEnd/App.js
@@ -4,6 +4,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from './Screens/LoginScreen';
 import SignupScreen from './Screens/SignupScreen';
 import ForgotPasswordScreen from './Screens/ForgotPasswordScreen';
+import MainScreen from './Screens/MainScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -12,14 +13,18 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Login" 
-        component={LoginScreen} 
-        options={{headerShown:false}}/>
+          component={LoginScreen} 
+          options={{headerShown:false}}/>
         <Stack.Screen name="Signup" 
-        component={SignupScreen}
+          component={SignupScreen}
          />
         <Stack.Screen 
-        name="ForgotPassword" 
-        component={ForgotPasswordScreen} />
+          name="ForgotPassword" 
+          component={ForgotPasswordScreen} />
+        <Stack.Screen name="MainScreen"
+          component={MainScreen}
+          options={{headerShown:false}}
+         />
       </Stack.Navigator>
     </NavigationContainer>
   );
