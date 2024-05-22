d14823e5acf011ca0c8823b133bb5dcecca6f566
Kleda Gashi <kledagashi1@gmail.com>
2024-05-23 01:03:42 +0200

    TQ-6: payment model method

--- BackEnd/models/payment.py
+++ BackEnd/models/payment.py
@@ -7,3 +7,6 @@ class Payment(models.Model):
     paymentType = models.CharField(max_length=50)  #'credit card', 'paypal', etc.
     booking = models.ForeignKey(Booking, on_delete=models.CASCADE)
     active = models.BooleanField(default=True)
+
+ def str(self):
+        return f"Payment {self.paymentid} for Booking {self.booking} with Type {self.paymentType}"
