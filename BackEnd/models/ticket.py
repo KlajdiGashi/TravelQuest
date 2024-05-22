2b3e4f6b795c1ff2e1ac07cf111631dad4df0ceb
Kleda Gashi <kledagashi1@gmail.com>
2024-05-23 00:47:26 +0200

    TQ-6: ticket model class

---
+++ BackEnd/models/ticket.py
@@ -0,0 +1,14 @@
+from django.db import models
+import uuid
+from .vendor import Vendor
+
+class Ticket(models.Model):
+    tid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    tfrom = models.CharField(max_length=255)
+    tend = models.CharField(max_length=255)
+    startTime = models.DateTimeField()
+    endTime = models.DateTimeField()
+    tprice = models.DecimalField(max_digits=10, decimal_places=2)
+    tvendorid = models.ForeignKey(Vendor, on_delete=models.CASCADE)
+    type = models.CharField(max_length=50)  # e.g., 'economy', 'business', etc.
+    tseat = models.CharField(max_length=10)  # e.g., '12A', '14C', etc.
