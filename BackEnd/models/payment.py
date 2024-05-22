9413be9e632860796f090bcde2d3c624f8e80a78
Kleda Gashi <kledagashi1@gmail.com>
2024-05-23 01:02:52 +0200

    TQ-6: payment model class

---
+++ BackEnd/models/payment.py
@@ -0,0 +1,9 @@
+from django.db import models
+import uuid
+from .bookings import Booking
+
+class Payment(models.Model):
+    paymentid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    paymentType = models.CharField(max_length=50)  #'credit card', 'paypal', etc.
+    booking = models.ForeignKey(Booking, on_delete=models.CASCADE)
+    active = models.BooleanField(default=True)
