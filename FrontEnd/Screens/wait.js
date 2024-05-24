f3e1157b366a8c274faf83d212cc0eae283cc2ae
rinesan <117470993+rinesan@users.noreply.github.com>
2024-05-24 13:33:43 +0200

    adding the wait screen and style

---
+++ FrontEnd/Screens/wait.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { View, Text, StyleSheet, ActivityIndicator } from 'react-native';
+
+const WaitScreen = () => {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.message}>Please wait while we're logging you in...</Text>
+            <ActivityIndicator size="large" color="#0000ff" />
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        backgroundColor: '#fff',
+    },
+    message: {
+        fontSize: 18,
+        marginBottom: 20,
+    },
+});
+
+export default WaitScreen;
