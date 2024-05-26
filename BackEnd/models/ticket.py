f1a44975a0a98a583784ebf4d02d80555c25c7ae
Kleda Gashi <kledagashi1@gmail.com>
2024-05-26 22:50:28 +0200

    TQ-6: update ticket model columns based on schema

--- BackEnd/models/ticket.py
+++ BackEnd/models/ticket.py
@@ -3,15 +3,15 @@ import uuid
 from .vendor import Vendor
 
 class Ticket(models.Model):
-    tid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    tfrom = models.CharField(max_length=255)
-    tend = models.CharField(max_length=255)
+    from_location = models.CharField(max_length=128)
+    to_location = models.CharField(max_length=128)
     startTime = models.DateTimeField()
     endTime = models.DateTimeField()
-    tprice = models.DecimalField(max_digits=10, decimal_places=2)
-    tvendorid = models.ForeignKey(Vendor, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
     type = models.CharField(max_length=50)  # e.g., 'economy', 'business', etc.
-    tseat = models.CharField(max_length=10)  # e.g., '12A', '14C', etc.
+    seat = models.CharField(max_length=10)  # e.g., '12A', '14C', etc.
+    vendor_id = models.ForeignKey(Vendor, on_delete=models.CASCADE)
+    image = models.ForeignKey(upload_to='ticket_images', null=True, blank=True)
 
     def __str__(self):
         return f"{self.tfrom} to {self.tend} on {self.startTime}"
