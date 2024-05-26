2f14dbc7aabe089718151883ee30ac4f87cfb6fb
Kleda Gashi <kledagashi1@gmail.com>
2024-05-26 22:52:36 +0200

    TQ-6: update ticket model columns based on schema

--- BackEnd/models/ticket.py
+++ BackEnd/models/ticket.py
@@ -14,4 +14,4 @@ class Ticket(models.Model):
     image = models.ForeignKey(upload_to='ticket_images', null=True, blank=True)
 
     def __str__(self):
-        return f"{self.tfrom} to {self.tend} on {self.startTime}"
+        return f"{self.from_location} to {self.to_location} on {self.seat}"
