2618ee4d783d4491748e0fa98f0deeb8e9c982fe
Kleda Gashi <kledagashi1@gmail.com>
2024-05-26 22:54:40 +0200

    TQ-6: update vendor model columns based on schema

--- BackEnd/models/vendor.py
+++ BackEnd/models/vendor.py
@@ -2,7 +2,6 @@ from django.db import models
 import uuid
 
 class Vendor(models.Model):
-    vid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     vendorname = models.CharField(max_length=255)
 
     def __str__(self):
