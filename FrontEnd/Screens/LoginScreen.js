e76902ead4c042c1e08cf480878fa8405b5a9db1
Klajdi Gashi <klajdi.gashi@student.uni-pr.edu>
2024-05-27 16:05:17 +0200

    TQ-18:Login API implementation

--- FrontEnd/Screens/LoginScreen.js
+++ FrontEnd/Screens/LoginScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, TouchableOpacity, ImageBackground,Image} from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, ImageBackground, Image } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
 
@@ -7,21 +7,48 @@ export default function LoginScreen({ navigation }) {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [showPassword, setShowPassword] = useState(false);
-
-  const handleLogin = () => {
-    // API here (Very important)
-
-    if (username.trim() === 'admin' || password.trim() === 'password') {
-      navigation.navigate('MainScreen');
-      // Added only for testing to see if the navigation works properly 
-    } else {
-      alert('Invalid username or password. Please try again.');
+  
+  const loginUser = async (username, password) => {
+    try {
+      const response = await fetch(`http://127.0.0.1:8000/api/user?username=${encodeURIComponent(username)}&password=${encodeURIComponent(password)}`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error('Error:', error);
+      throw new Error('An error occurred. Please try again later.');
     }
   };
+  
+  const handleLogin = async () => {
+    if (username.trim() === '' || password.trim() === '') {
+      alert('Please enter both username and password.');
+      return;
+    }
+  
+    try {
+      let data = await loginUser(username.trim(), password.trim());
+      console.log(data);
+      if (data.guid) { // or check another field to confirm successful login
+        alert('Login successful!');
+        navigation.navigate('MainScreen', { userData: data });
+      } else {
+        alert('Invalid username or password. Please try again.');
+      }
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+  
 
   const handleSignup = () => {
     navigation.navigate('Signup');
   };
+
   const handleForgotPassword = () => {
     navigation.navigate('ForgotPassword');
   };
@@ -30,15 +57,15 @@ export default function LoginScreen({ navigation }) {
     <ImageBackground source={require('../assets/BackgroundImage.png')} style={styles.container}>
       <View style={styles.logoContainer}>
         <Image
-          source={ require('../assets/Logo.png') }
-          style={{ width: 200, height: 100, borderRadius: 10, bottom: 60, shadowOffset: { width: 0, height: 2 }, shadowOpacity: 2, shadowRadius: 4.84, elevation: 5 }}
+          source={require('../assets/Logo.png')}
+          style={styles.logo}
           resizeMode="contain"
         />
       </View>
       <View style={styles.inputContainer}>
         <Text style={styles.label}>Username:</Text>
         <TextInput
-          style={[styles.input, { backgroundColor: '#F5F5F5' }]}
+          style={styles.input}
           onChangeText={setUsername}
           value={username}
           placeholder="Enter your username"
@@ -47,18 +74,15 @@ export default function LoginScreen({ navigation }) {
       </View>
       <View style={styles.inputContainer}>
         <Text style={styles.label}>Password:</Text>
-        <View style={[styles.passwordContainer, { backgroundColor: '#F5F5F5' }]}>
+        <View style={styles.passwordContainer}>
           <TextInput
-            style={[styles.passwordInput, { backgroundColor: '#F5F5F5' }]}
+            style={styles.passwordInput}
             onChangeText={setPassword}
             value={password}
             placeholder="Enter your password"
             secureTextEntry={!showPassword}
           />
-          <TouchableOpacity
-            onPress={() => setShowPassword(!showPassword)}
-            style={styles.eyeIcon}
-          >
+          <TouchableOpacity onPress={() => setShowPassword(!showPassword)} style={styles.eyeIcon}>
             <Ionicons name={showPassword ? 'eye-off' : 'eye'} size={24} color="black" />
           </TouchableOpacity>
         </View>
@@ -68,7 +92,7 @@ export default function LoginScreen({ navigation }) {
       </TouchableOpacity>
       <View style={styles.buttonContainer}>
         <TouchableOpacity onPress={handleSignup}>
-          <Text style={styles.SignUpText}>Sign Up</Text>
+          <Text style={styles.signupText}>Sign Up</Text>
         </TouchableOpacity>
         <TouchableOpacity onPress={handleForgotPassword}>
           <Text style={styles.forgotPasswordText}>Forgot Password?</Text>
@@ -89,12 +113,23 @@ const styles = StyleSheet.create({
   logoContainer: {
     marginBottom: 20,
   },
+  logo: {
+    width: 200,
+    height: 100,
+    borderRadius: 10,
+    bottom: 60,
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 2,
+    shadowRadius: 4.84,
+    elevation: 5,
+  },
   inputContainer: {
     width: '100%',
     marginBottom: 20,
   },
   label: {
     marginBottom: 5,
+    color: 'black',
   },
   input: {
     width: '100%',
@@ -110,7 +145,7 @@ const styles = StyleSheet.create({
       height: 2,
     },
     shadowOpacity: 2,
-    shadowRadius:4.84,
+    shadowRadius: 4.84,
     elevation: 5,
   },
   passwordContainer: {
@@ -141,7 +176,7 @@ const styles = StyleSheet.create({
   },
   passwordInput: {
     flex: 1,
-    height: 50
+    height: 50,
   },
   eyeIcon: {
     position: 'absolute',
@@ -163,12 +198,10 @@ const styles = StyleSheet.create({
     color: '#F5F5F5',
     textDecorationLine: 'underline',
     fontSize: 16,
-
   },
-  SignUpText: {
+  signupText: {
     color: '#F5F5F5',
     textDecorationLine: 'underline',
     fontSize: 16,
-
   },
 });
